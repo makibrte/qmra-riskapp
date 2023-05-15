fd3fd874e99327b410db90071210bedf2dcba746
matejam <113369108+makibrte@users.noreply.github.com>
2023-05-15 19:35:29 -0400

    added option to choose between distributions

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 import streamlit as st
 import numpy as np
+from scipy.stats import betaprime
 
-st.markdown("""
-<style>
-.big-font {
-    color: blue;
-    font-size:50px !important;
-}
-</style>
-""", unsafe_allow_html=True)
+st.title('Distribution Calculator')
 
-st.markdown('<p class="big-font">Exponential Distribution Risk Calculator</p>', unsafe_allow_html=True)
-
-st.latex('Risk = 1 - exp(-k * dose) \\\ Probability = 1 - (1- Risk)^{\\alpha}')
-
-
-
-# Define a function to calculate risk
-def calculate_risk(k, dose, alpha):
+# Define functions to calculate risk for each distribution
+def calculate_risk_exp(k, dose):
     risk = 1 - np.exp(-k * dose)
-    prob = 1-np.power((1-risk),alpha)
-    return prob
+    return risk
 
+def calculate_risk_beta_poisson(alpha, beta, dose):
+    risk = 1 - np.exp(-betaprime.pdf(dose, alpha, beta))
+    return risk
 
-k = st.number_input('Enter the value for k')
-dose = st.number_input('Enter the value for dose')
-alpha = st.number_input('Enter alpha')
-if st.button('Calculate Risk'):
-    
-    risk = calculate_risk(k, dose,alpha)
+# Use a selectbox to choose the distribution
+distributions = st.select_slider(
+    'Select a color of the rainbow',
+    options=['Exponential', 'Beta-Poisson'])
+#distribution = st.selectbox('Choose a distribution', ('Exponential', 'Beta-Poisson'))
 
-    
-    st.write(f'The calculated risk is: {risk:.4f}')
+if distributions == 'Exponential':
+    # Use sliders to select parameters for exponential distribution
+    k = st.slider('Enter the value for k', min_value=0.0, max_value=10.0, value=1.0, step=0.1)
+    dose = st.slider('Enter the value for dose', min_value=0.0, max_value=100.0, value=10.0, step=0.1)
+
+    if st.button('Calculate Risk for Exponential'):
+        # Calculate risk
+        risk = calculate_risk_exp(k, dose)
+
+        # Display risk
+        st.write(f'The calculated risk for exponential distribution is: {risk:.4f}')
+
+elif distributions == 'Beta-Poisson':
+    # Use sliders to select parameters for beta-poisson distribution
+    alpha = st.slider('Enter the value for alpha', min_value=0.0, max_value=10.0, value=1.0, step=0.1)
+    beta = st.slider('Enter the value for beta', min_value=0.0, max_value=10.0, value=1.0, step=0.1)
+    dose = st.slider('Enter the value for dose', min_value=0.0, max_value=100.0, value=10.0, step=0.1)
+
+    if st.button('Calculate Risk for Beta-Poisson'):
+        # Calculate risk
+        risk = calculate_risk_beta_poisson(alpha, beta, dose)
+
+        # Display risk
+        st.write(f'The calculated risk for beta-poisson distribution is: {risk:.4f}')
