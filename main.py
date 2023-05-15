fix the formula

main.py
@@ -14,14 +14,15 @@ st.markdown('<p class="big-font">Exponential Distribution Risk Calculator</p>', 
 
 
 # Define a function to calculate risk
-def calculate_risk(k, dose):
+def calculate_risk(k, dose, alpha):
     risk = 1 - np.exp(-k * dose)
-    return risk
+    prob = 1-(1-risk)**alpha
+    return prob
 
 
 k = st.number_input('Enter the value for k')
 dose = st.number_input('Enter the value for dose')
-
+alpha = st.number_input('Enter alpha')
 if st.button('Calculate Risk'):
     
     risk = calculate_risk(k, dose)