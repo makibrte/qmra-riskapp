de2a3f9e7eebf89a33dad4d52e24e39abc72444c
matejam <113369108+makibrte@users.noreply.github.com>
2023-05-15 18:04:37 -0400

    fixed LaTeX

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ st.markdown("""
 
 st.markdown('<p class="big-font">Exponential Distribution Risk Calculator</p>', unsafe_allow_html=True)
 
-st.latex('Risk = 1 - exp(-k * dose)')
-st.latex("Probability = 1 - (1- Risk)^{\alpha}")
+st.latex('Risk = 1 - exp(-k * dose) \\* Probability = 1 - (1- Risk)^{\alpha}')
+
+
 
 # Define a function to calculate risk
 def calculate_risk(k, dose, alpha):
