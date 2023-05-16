f88d69779d62446c63ee94f5ec434c895f6412b0
matejam <113369108+makibrte@users.noreply.github.com>
2023-05-16 16:10:03 -0400

    added back latex formulas

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ distributions = st.select_slider(
 #distribution = st.selectbox('Choose a distribution', ('Exponential', 'Beta-Poisson'))
 
 if distributions == 'Exponential':
+    st.latex("1 - (-k \\times dose)")
     # Use sliders to select parameters for exponential distribution
     k = st.slider('Enter the value for k', min_value=0.0, max_value=10.0, value=1.0, step=0.1)
     dose = st.slider('Enter the value for dose', min_value=0.0, max_value=100.0, value=10.0, step=0.1)
@@ -32,6 +33,7 @@ if distributions == 'Exponential':
         st.write(f'The calculated risk for exponential distribution is: {risk:.4f}')
 
 elif distributions == 'Beta-Poisson':
+    st.latex("1 - [1 + \\frac{dose}{\\beta}]^{-\\alpha}")
     # Use sliders to select parameters for beta-poisson distribution
     alpha = st.slider('Enter the value for alpha', min_value=0.0, max_value=10.0, value=1.0, step=0.1)
     beta = st.slider('Enter the value for beta', min_value=0.0, max_value=10.0, value=1.0, step=0.1)
